7669a7bebe3b324ec3a15559a793bd6ac32e370d
Michaël Roy <microygh@gmail.com>
2021-10-21 13:28:07 +0200

    Use QUdpSocket. Update and cleanup.

--- Chat/qt-multicast-chat.py
+++ Chat/qt-multicast-chat.py
@@ -1,108 +1,73 @@
 #! /usr/bin/env python3
 
 #
-# Multicast Chat Application with Qt interface
+# Multicast Chat Application using Qt
 # https://github.com/rtauxerre/Multicast
 # Copyright (c) 2021 Michaël Roy
 #
 
 # External dependencies
 import os
-import select
-import socket
 import sys
-import time
-import threading
-from PySide2.QtCore import *
-from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from PySide2.QtNetwork import *
+from PySide2.QtWidgets import *
 
-#
 # Multicast address and port
-#
 multicast_address = '239.0.0.1'
 multicast_port = 10000
 
-#
-# Chat Server
-#
-class ChatServer( threading.Thread ) :
-	# Server main loop
-	def run( self ) :
-		# Set up the server connection
-		connection = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-		connection.setsockopt( socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
-				socket.inet_aton( multicast_address ) + socket.inet_aton( '0.0.0.0' ) )
-		connection.bind( ( '0.0.0.0', multicast_port ) )
-		# Continuously read client message
-		self.running = True
-		while self.running :
-			# Wait for a message
-			ready, _, _ = select.select( [ connection ], [], [], 0 )
-			# Message received
-			if ready :
-				# Read the message
-				message, address = connection.recvfrom( 255 )
-				global window
-				window.text_area.append('{} : {} > {}'.format( address[0], address[1], message.decode() ))
-			# Temporization
-			time.sleep( 0.03 )
-		# Close the connection
-		connection.close()
-
-#
-# Chat client with Qt interface
-#
+# Multicast Chat using Qt
 class QMulticastChat( QWidget ) :
 	# Initialize the window
-	def __init__( self ):
+	def __init__( self ) :
+		# Initialize the class
 		QWidget.__init__( self )
 		# Set the window title
 		self.setWindowTitle( 'RT Auxerre Multicast Chat' )
 		# Set fixed window size
 		self.setFixedWidth(800)
 		self.setFixedHeight(600)
-		# Text edit to show the received messages
-		self.text_area = QTextEdit()
-		self.text_area.setFocusPolicy( Qt.NoFocus )
-		# Line edit to enter the message to send
-		self.message = QLineEdit()
-		# Application layout
-		self.layout = QVBoxLayout( self )
-		self.layout.addWidget( QLabel('Message received :') )
-		self.layout.addWidget( self.text_area )
-		self.layout.addWidget( QLabel('Send a message :') )
-		self.layout.addWidget( self.message )
-		# Signal to know when a new message is entered
-		self.message.returnPressed.connect( self.send_message )
 		# Set the Escape key to close the application
 		QShortcut( QKeySequence( Qt.Key_Escape ), self ).activated.connect( self.close )
-		# Network connection to send the messages
-		self.connection = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-		self.connection.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1 )
-		# Start the server
-		self.server = ChatServer()
-		self.server.start()
-	# Handle a new message
+		# Text edit to show the received messages
+		self.chat = QTextEdit()
+		self.chat.setFocusPolicy( Qt.NoFocus )
+		# Line edit to enter the message to send
+		self.message = QLineEdit()
+		# Signal to know when a new message is entered
+		self.message.returnPressed.connect( self.send_message )
+		# Application layout
+		self.layout = QVBoxLayout( self )
+		self.layout.addWidget( QLabel( 'Message received :' ) )
+		self.layout.addWidget( self.chat )
+		self.layout.addWidget( QLabel( 'Send a message :' ) )
+		self.layout.addWidget( self.message )
+		# Server connection to receive messages
+		self.server = QUdpSocket( self )
+		self.server.bind( QHostAddress( '' ), multicast_port )
+		self.server.joinMulticastGroup( QHostAddress( multicast_address ) )
+		self.server.readyRead.connect( self.receive_message )
+		# Client connection to send messages
+		self.client = QUdpSocket( self )
+	# Send a message
 	def send_message( self ) :
 		# Return if the message is empty
 		if not self.message.text() : return
 		# Send the message through the network
-		self.connection.sendto( self.message.text().encode(), ( multicast_address, multicast_port ) )
+		self.client.writeDatagram( self.message.text().encode(), QHostAddress( multicast_address ), multicast_port )
 		# Clear the text input widget
 		self.message.clear()
-	# Close the widget
-	def closeEvent( self, event ) :
-		# Close the server
-		self.server.running = False
-		# Close the client connection
-		self.connection.close()
-		# Close main application
-		event.accept()
+	# Receive the messages
+	def receive_message( self ) :
+		# Handle all the pending messages
+		while self.server.hasPendingDatagrams() :
+			# Get the message
+			message, host, port = self.server.readDatagram( self.server.pendingDatagramSize() )
+			# Print the message
+			self.chat.append( '{} : {} > {}'.format( host.toString(), port, message.data().decode() ) )
 
-#
-# Main application
-#
+# Main program
 if __name__ == "__main__" :
 	# Remove Qt warnings
 	os.environ["QT_DEVICE_PIXEL_RATIO"] = "0"
@@ -110,7 +75,7 @@ if __name__ == "__main__" :
 	os.environ["QT_SCREEN_SCALE_FACTORS"] = "1"
 	os.environ["QT_SCALE_FACTOR"] = "1"
 	# Start the Qt application
-	app = QApplication( [] )
+	application = QApplication( sys.argv )
 	window = QMulticastChat()
 	window.show()
-	sys.exit( app.exec_() )
+	sys.exit( application.exec_() )
