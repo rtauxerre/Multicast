41cfadcb56e08a82bfb7189a6f2aaf0e31bddf3e
Michaël Roy <microygh@gmail.com>
2021-10-21 18:36:07 +0200

    Input loop to type several messages.

--- Chat/sender.py
+++ Chat/sender.py
@@ -4,23 +4,26 @@
 # Multicast Chat Application - Client implementation
 # https://github.com/rtauxerre/Multicast
 # Copyright (c) 2021 Michaël Roy
-# usage : $ ./sender.py message
+# usage : $ ./sender.py
 #
 
 # External dependencies
 import socket
-import sys
 
 # Multicast address and port
 multicast_address = '239.0.0.1'
 multicast_port = 10000
 
-# Check the message
-if len( sys.argv ) > 1 : message = sys.argv[ 1 ]
-else : print( 'Message missing...' ); exit( 1 )
-
-# Create a UDP socket
-with socket.socket( socket.AF_INET, socket.SOCK_DGRAM ) as connection :
-	# Send the message to the multicast address and port
-	connection.sendto( message.encode(), ( multicast_address, multicast_port ) )
-
+# Handle exceptions such as Ctrl+C
+try :
+	# Create a UDP socket
+	connection = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+	while True :
+		# Get the message from console input
+		message = input( '> ' )
+		# Send the message to the multicast address and port
+		connection.sendto( message.encode(), ( multicast_address, multicast_port ) )
+# Exceptions
+except : pass
+# Close the connection
+finally : connection.close()
