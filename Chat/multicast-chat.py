29d5b2dfb11d3821cc1b08f4c1dc04f70a2659bf
Michaël Roy <microygh@gmail.com>
2021-10-26 18:03:51 +0200

    Remove the server and client classes.

--- Chat/multicast-chat.py
+++ Chat/multicast-chat.py
@@ -8,85 +8,49 @@
 #
 
 # External dependencies
-import select
 import socket
-import time
 import threading
 
-# Multicast address
+# Multicast address and port
 multicast_address = '239.0.0.1'
-
-# Multicast port
 multicast_port = 10000
 
-#
 # Chat Server
-#
-class ChatServer( threading.Thread ) :
-	# Server main loop
-	def run( self ) :
+def Server() :
+	# Create a UDP socket
+	with socket.socket( socket.AF_INET, socket.SOCK_DGRAM ) as connection :
 		# Set up the server connection
-		connection = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 		connection.setsockopt( socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
 				socket.inet_aton( multicast_address ) + socket.inet_aton( '0.0.0.0' ) )
 		connection.bind( ( '0.0.0.0', multicast_port ) )
 		# Continuously read client message
-		self.running = True
-		while self.running :
+		while True :
 			# Wait for a message
-			ready, _, _ = select.select( [ connection ], [], [], 0 )
-			# Message received
-			if ready :
-				# Read the message
-				message, address = connection.recvfrom( 255 )
-				# Print the message
-				print( 'Received from {} : {} > {}'.format( *address, message.decode() ) )
-			# Temporization
-			time.sleep( 0.1 )
-		# Close the connection
-		connection.close()
+			message, address = connection.recvfrom( 4096 )
+			# Print the message
+			print( '{} : {} > {}'.format( *address, message.decode() ) )
 
-#
 # Chat client
-#
-class ChatClient( threading.Thread ) :
-	# Client main loop
-	def run( self ) :
-		# Create a UDP socket
-		connection = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+def Client() :
+	# Create a UDP socket
+	with socket.socket( socket.AF_INET, socket.SOCK_DGRAM ) as connection :
 		# Continuously read the messages from input and send them
-		self.running = True
-		while self.running :
+		while True :
 			# Get the message from input
 			message = input( '> ' )
 			# Check if the message is empty
 			if not message : continue
 			# Check if the message is the 'quit' command
-			if message == "quit" :
-				# Stop the client
-				self.running = False
-				# Get the server
-				global server
-				# Stop the server
-				server.running = False
-				break
+			if message == "quit" : break
 			# Send the message through the network
 			connection.sendto( message.encode(), ( multicast_address, multicast_port ) )
-			# Temporization
-			time.sleep( 0.1 )
-		# Close the connection
-		connection.close()
 
-#
 # Main application
-#
 if __name__ == '__main__' :
 	# Banner
 	print( '\nWelcome to the multicast chat application from RT Auxerre !')
 	print( 'Type \'quit\' to stop the application...\n' )
 	# Start the server
-	server = ChatServer()
-	server.start()
+	threading.Thread( target=Server, daemon=True ).start()
 	# Start the client
-	client = ChatClient()
-	client.start()
+	threading.Thread( target=Client ).start()
