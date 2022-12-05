442ff22cc59c8657ead4f98b39cdaa359bdb1c6d
Michaël Roy <microygh@gmail.com>
2022-12-05 14:39:50 +0100

    Improve async receiver.

--- Chat/receiver-async.py
+++ Chat/receiver-async.py
@@ -3,8 +3,8 @@
 #
 # Multicast Chat Application - Server implementation
 # https://github.com/rtauxerre/Multicast
-# Copyright (c) 2021 Michaël Roy
-# usage : $ ./receiver.py
+# Copyright (c) 2022 Michaël Roy
+# usage : $ ./receiver-async.py
 #
 
 # External dependencies
@@ -15,25 +15,24 @@ import socket
 multicast_address = '239.0.0.1'
 multicast_port = 10000
 
-
-class EchoServerProtocol :
+# Chat server protocol
+class ChatServer :
+	# Connection
 	def connection_made( self, transport ) :
 		self.transport = transport
 		self.transport.get_extra_info('socket').setsockopt( socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
 			socket.inet_aton( multicast_address ) + socket.inet_aton( '0.0.0.0' ) )
+	# Message reception
 	def datagram_received( self, message, address ) :
 		print( '{} : {} > {}'.format( *address, message.decode() ) )
 
-async def main() :
-	# Get a reference to the event loop as we plan to use low-level APIs.
-	loop = asyncio.get_running_loop()
-	# One protocol instance will be created to serve all client requests.
-	transport, protocol = await loop.create_datagram_endpoint(
-		lambda: EchoServerProtocol(),
-		local_addr=('0.0.0.0', multicast_port) )
-	try:
-		await asyncio.sleep(3600) # Serve for 1 hour.
-	finally:
-		transport.close()
-
-asyncio.run( main() )
+# Main application
+if __name__ == '__main__' :
+	print( '\nPress ^C to stop the server...\n' )
+	loop = asyncio.get_event_loop()
+	server = loop.create_datagram_endpoint( lambda: ChatServer(), local_addr=('::', multicast_port) )
+	loop.run_until_complete( server )
+	try :
+		loop.run_forever()
+	except KeyboardInterrupt :
+		pass
